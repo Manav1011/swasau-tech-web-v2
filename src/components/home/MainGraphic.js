64b3dd6a84d914d092810fa7f443e28edc92c8e2
Manav1011 <manavshah1011.ms@gmail.com>
2024-01-25 00:11:34 +0530

    added main graphics

---
+++ src/components/home/MainGraphic.js
@@ -0,0 +1,29 @@
+async function MainGraphic({grad}) {
+    const graphics_arr = [
+        "Collaboration-cuate.svg",
+        "Printed-circuit-board-amico.svg",
+        "Printed-circuit-board-rafiki.svg",
+        "Creative-team-bro.svg",
+        "Printed-circuit-board-bro.svg",
+        "Process-cuate.svg",
+        "Hand-coding-pana.svg",
+        "Printed-circuit-board-cuate.svg",
+        "Server-status-cuate.svg",        
+        "Printed-circuit-board-pana.svg",
+    ]
+    const choosed_graphic_index = Math.floor(Math.random() * graphics_arr.length)    
+  return (
+    <div className={`${grad}`}>
+      <div className="justify-center text-center  sm:mx-20 mx-5 bg-center sm:bg-right your-custom-class flex  flex-col" style={{ height: 'calc(100vh - 5rem)', backgroundImage: `url(/images/main-graphics/${graphics_arr[choosed_graphic_index]})`, backgroundRepeat:'no-repeat'}}>
+          <div className='backdrop-filter backdrop-blur-sm bg-opacity-20 rounded-md bg-clip-padding p-10 bg-gray-400 border border-gray-100 text-center xl:border-0 xl:backdrop-blur-0 xl:bg-opacity-100 xl:text-left xl:bg-transparent'>
+            <h1 className="block w-50 text-black text-5xl font-extrabold" style={{fontFamily:'Azonix'}}>Swasau Technology</h1>
+            <div className="mt-3 text-3xl block font-medium" style={{fontFamily:'MoonLight'}}>
+                Creating Dream Sollutions
+            </div>
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default MainGraphic
