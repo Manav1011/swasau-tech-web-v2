752baadc63f4e003ecc54004b5c94db0a0472a9d
Manav1011 <manavshah1011.ms@gmail.com>
2024-01-26 14:49:08 +0530

    added animations to be bottom up

--- src/components/home/ServiceCard.js
+++ src/components/home/ServiceCard.js
@@ -11,7 +11,7 @@ const ServiceCard = ({ icon, title }) => {
     return (
       <>
         <div
-            className={`${inView?'left-right':null} hover:scale-110 transition ease-in-out delay-100 duration-300 relative sm:m-10 m-5 grid h-[15rem] w-full max-w-[15rem] flex-col items-end justify-center overflow-hidden rounded-xl bg-white bg-clip-border text-center text-gray-700`} ref={myref}>
+            className={`${inView?'bottom-up':null} hover:scale-110 transition ease-in-out delay-100 duration-300 relative sm:m-10 m-5 grid h-[15rem] w-full max-w-[15rem] flex-col items-end justify-center overflow-hidden rounded-xl bg-white bg-clip-border text-center text-gray-700`} ref={myref}>
             <div
               className="absolute inset-0 m-0 h-full w-full overflow-hidden rounded-none bg-transparent bg-cover bg-clip-border bg-center text-gray-700 shadow-none" style={backgroundImageStyle}>
               <div className="absolute inset-0 w-full h-full to-bg-black-10 bg-gradient-to-t from-black/80 via-black/50"></div>
--- src/components/home/TeamMemberCard.js
+++ src/components/home/TeamMemberCard.js
@@ -8,7 +8,7 @@ function TeamMemberCard(props) {
     const { ref: myref, inView, entry } = useInView(options);
     const {id,dp,name,designation,github,linkedin} = props     
   return (
-    <div className={`${inView ? "right-left" : null} hover:scale-110 transition ease-in-out delay-100 duration-300 mb-10 text-center text-gray-500 dark:text-gray-400`} ref={myref}>
+    <div className={`${inView ? "bottom-up" : null} hover:scale-110 transition ease-in-out delay-100 duration-300 mb-10 text-center text-gray-500 dark:text-gray-400`} ref={myref}>
         <Link href={`/team/${id}`} aria-current="page">
         <img className="mx-auto mb-3 w-36 h-36 rounded-full mix-blend-darken" src={dp} alt="Bonnie Avatar" />
         <h3 className="text-xl font-mono font-medium text-slate-900">
