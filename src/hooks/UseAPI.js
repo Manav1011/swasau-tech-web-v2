112c8c4ec32738cf8fef0afa461c7895f0b93969
Manav1011 <manavshah1011.ms@gmail.com>
2024-01-28 12:50:38 +0530

    added no cache to fetch

--- src/hooks/UseAPI.js
+++ src/hooks/UseAPI.js
@@ -1,28 +1,34 @@
 import axios from "axios";
 async function UseAPI(query, variables) {
-  let res = {
-    error: false,
-    data: null,
-  };  
-  try {
-    const response = await axios.post(
-      'https://graphql.contentful.com/content/v1/spaces/szk2cuaoivwt/environments/master',
-      {
-        query: query,
-        variables: variables,
-      },
-      {
-        headers: {          
+    let res = {
+      error: false,
+      data: null,
+    };
+    try {
+      const response = await fetch('https://graphql.contentful.com/content/v1/spaces/szk2cuaoivwt/environments/master', {
+        cache: 'no-cache',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
           'Authorization': `Bearer ${process.env.CONTENTFUL_AUTH}`,
         },
+        body: JSON.stringify({
+          query: query,
+          variables: variables,
+        }),
+      });
+    
+      const responseData = await response.json();
+    
+      if (!response.ok) {
+        throw new Error(responseData.message || 'Request failed');
       }
-    );
-
-    res.data = response.data.data;
-  } catch (error) {
-    res.error = true;
-  }  
-  return res;
+    
+      res.data = responseData.data;
+    } catch (error) {
+      res.error = true;
+    }  
+    return res;  
 }
 
 export default UseAPI;
